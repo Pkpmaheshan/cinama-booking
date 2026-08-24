cc970fc8d88340c215659fedc58c5a559a2fdc28
Pkpmaheshan <panchayumaheshan@gmail.com>
2026-08-24 09:01:50 +0530

    Fix lint configuration and test setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,16 +3,22 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default [
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
   {
     files: ["src/**/*.ts"],
+
     languageOptions: {
       globals: {
         ...globals.node,
       },
     },
+
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
   },
-
-  js.configs.recommended,
-
-  ...tseslint.configs.recommended,
 ];
